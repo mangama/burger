0dc777e40c4b736a31b7cba94563f6d31c4b0b9f
Christelle Mangama <mangama_christelle@yahoo.fr>
2019-07-24 14:59:39 -0600

    Adding changes from comaparing with the catApp

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # burger
+
+
+
+Have burgers in the system.
+Clicking on the devour an burger it is just as if you were sleepying \
+
+
+
+exept for the 
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Colorado Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('One Pound Burger');
